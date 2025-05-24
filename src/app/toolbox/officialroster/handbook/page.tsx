58d40d87c3f86f578c1e1165a6ca50372b82c3b5
Charles Lang <langcharles08@gmail.com>
2025-05-24 02:26:05 +0000

    sorry, change it to this: https://docs.google.com/document/d/18R1-YIN3JhPJ-3gw0_W5JZOJeDYfM7e-/edit?usp=sharing&ouid=116424317365062189721&rtpof=true&sd=true

--- src/app/toolbox/officialroster/handbook/page.tsx
+++ src/app/toolbox/officialroster/handbook/page.tsx
@@ -7,7 +7,7 @@ import { NavbarTools } from '@/components/NavbarTools';
 
 export default function AssignorHandbookPage() {
   // Use the /view URL for Google Drive for better embed compatibility
-  const documentUrl = "https://drive.google.com/file/d/1ORFf0wJ-fwuGhgSBC9iMukbohSjcXJ8e/view";
+  const documentUrl = "https://docs.google.com/document/d/18R1-YIN3JhPJ-3gw0_W5JZOJeDYfM7e-/view";
 
   return (
     <div className="flex flex-col h-screen items-center mx-auto max-w-[500px]">
